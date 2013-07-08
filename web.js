3b32dc913100b83dfee3ac0dceed1c452d718190
Jose Viegas <jose.viegas@besteam.ch>
2013-07-08 15:29:17 +0000

    testing commit

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require("fs");
+var file = fs.readFileSync('index.html');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Welcome World to my lair!!');
+  response.send(file.toString('utf8'));
 });
 
 var port = process.env.PORT || 5000;
