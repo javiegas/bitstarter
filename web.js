1d164b26d052f302f213a057ab08a9952f1d0967
Ubuntu <ubuntu@ip-172-31-32-124.us-west-2.compute.internal>
2013-07-31 16:18:56 +0000

    New webpage for Bitstart

--- web.js
+++ web.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var fs = require("fs");
+var util = require('util');
 var file = fs.readFileSync('index.html');
+var other  = fs.readFileSync('Product.png');
 
 var app = express.createServer(express.logger());
 
@@ -8,7 +10,21 @@ app.get('/', function(request, response) {
   response.send(file.toString('utf8'));
 });
 
-var port = process.env.PORT || 5000;
+app.get('/Product.png', function(request, response) {
+  fs.stat('Product.png', function(error, stat) {
+    var rs;
+    response.writeHead(200, { 'Content-Type': 'image/png','Content-Length' : stat.size});
+    rs = fs.createReadStream('Product.png');
+    util.pump(rs, response, function(err) {
+      if (err) {
+        throw err;
+      }
+  });
+  });
+});
+
+
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
